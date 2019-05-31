c4a9e4b83bb33c33e748ffa24a0c57adf6403554
feeewq3 <16137238uca>
2019-05-31 08:19:28 -0600

    webservice

--- app/src/main/java/com/petrlr14/mvvm/database/repositories/GitHubRepoRepository.kt
+++ app/src/main/java/com/petrlr14/mvvm/database/repositories/GitHubRepoRepository.kt
@@ -2,10 +2,20 @@ package com.petrlr14.mvvm.database.repositories
 
 import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
+import com.petrlr14.mvvm.Service.retrofit.GithubService
 import com.petrlr14.mvvm.database.daos.GitHubDAO
 import com.petrlr14.mvvm.database.entities.GitHubRepo
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class GitHubRepoRepository (private val repoDao:GitHubDAO){
+
+    /*fun retrieveReposAsync(user:String):Deferred<Response<List<GitHubRepo>>> =
+       GithubService.getGithubService().getAllReposPerUser(user)*/
+    fun retrieveRepos(user:String) = GlobalScope.launch (Dispatchers.IO){  }
 
     @WorkerThread
     suspend fun insert(repo:GitHubRepo){
--- app/src/main/java/com/petrlr14/mvvm/database/viewmodels/GitHubRepoViewModel.kt
+++ app/src/main/java/com/petrlr14/mvvm/database/viewmodels/GitHubRepoViewModel.kt
@@ -1,11 +1,15 @@
 package com.petrlr14.mvvm.database.viewmodels
 
 import android.app.Application
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
 import com.petrlr14.mvvm.database.RoomDB
 import com.petrlr14.mvvm.database.entities.GitHubRepo
 import com.petrlr14.mvvm.database.repositories.GitHubRepoRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class GitHubRepoViewModel(private val app: Application) : AndroidViewModel(app) {
 
@@ -23,5 +27,25 @@ class GitHubRepoViewModel(private val app: Application) : AndroidViewModel(app) 
     }
 
     private suspend fun nuke()= repository.nuke()
+
+    fun retrieveRepos(user: String) = viewModelScope.launch(){
+        this@GitHubRepoViewModel.nuke()//TODO OBTENEMOS NUESTRA TABLA LIMPIA
+
+        val response = repository.retrieveReposAsync(user).await()
+
+        if (response.isSuccessful) with(response){
+            //TODO WITH TRABAJA CON LA VARIABLE QUE LE MANDAS
+            this.body()?.forEach{
+                this@GitHubRepoViewModel.insert(it)
+            }//TODO PUEDE QUE VENGA VACIO POR ESO EL NULO
+        }
+        else with(response){
+            when(this.code()){
+                404->{
+                    Toast.makeText(app, "User not found", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
 
 }
